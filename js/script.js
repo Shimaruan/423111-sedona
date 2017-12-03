af57aed18b433531e133ed4c38138fed542f47f7
Shimaruan <33229203+Shimaruan@users.noreply.github.com>
2017-12-03 18:07:44 +0300

    Добавлен js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+  var modalButton= document.querySelector(".btn-service");
+  var modal= document.querySelector(".search-hotels");
+  var modalForm= document.querySelector(".search-hotels-form");
+
+  modal.classList.remove("modal-show");
+
+  modalButton.addEventListener("click", function(){modal.classList.toggle("modal-show");});
+
+  modal.classList.remove("modal-show");
+
