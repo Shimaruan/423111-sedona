a67c9bf73f8a2509de4935192b3ff5b3eb4fe15a
Shimaruan <33229203+Shimaruan@users.noreply.github.com>
2017-12-05 21:15:10 +0300

    Правка

--- js/script.js
+++ js/script.js
@@ -1,20 +1,24 @@
 var modalButton= document.querySelector(".btn-service");
 var modal= document.querySelector(".search-hotels");
-var form=  modal.querySelector(".search-hotels-form");
-var arrivalDate= modal.querySelector("[name= arrival-date]");
-var departureDate= modal.querySelector("[name= departure-date]");
-var quantityAdult= modal.querySelector("[name= quantity-adult]");
-var quantityKids= modal.querySelector("[name= quantity-kids]");
+var sendButton= modal.querySelector(".search-btn");
+var arrival = document.querySelector("#arrival-date");
+var departure = document.querySelector("#departure-date");
+var adult = document.querySelector("#quantity-adult-id");
+var kids = document.querySelector("#quantity-kids-id");
+
 modal.classList.remove("modal-show");
+
 modalButton.addEventListener("click", function(evt) {
   evt.preventDefault();
   modal.classList.toggle("modal-show");
 });
-form.addEventListener("submit", function (evt) {
-  if (!arrivalDate.value || !departureDate.value || !quantityAdult.value || !quantityKids.value){
-    evt.preventDefault();
-    modal.classList.remove("modal-error");
-    modal.offsetWidth= modal.offsetWidth;
+
+sendButton.addEventListener("click", function(evt) {
+  modal.classList.remove("modal-error");
+  modal.offsetWidth = modal.offsetWidth;
+
+  if (!(arrival.value && departure.value && adult.value && kids.value)) {
+    event.preventDefault();
     modal.classList.add("modal-error");
-    }
+  }
 });
